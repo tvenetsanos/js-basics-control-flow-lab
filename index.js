5f65caa643865692726cf9b57a14bb1ea05a5f8b
Tyler <tyler_venetsanos@msn.com>
2021-04-06 15:24:47 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if(ride <= 400) {
+    return("This one is on me!");
+  }else if(ride > 2500){
+    return('No can do.')
+  }else if(ride > 2000){
+    return('I will gladly take your thirty bucks.')
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination) {
+  return destination === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(amount){
+  switch (amount) {
+    case "generous":
+      return("Thank you so much.")
+      break
+    case "not as generous":
+      return("Thank you.")  
+      break
+    default:
+      return("Bye.")
+
+  }
 }
